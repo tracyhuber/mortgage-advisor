9817073b2b20f94a6fd3692438cfc7e6c1ad5abc
Tracy Huber <tmhuber3@gmail.com>
2020-02-12 17:40:27 -0600

    created Client form

--- src/components/mortgage/ClientComponent.jsx
+++ src/components/mortgage/ClientComponent.jsx
@@ -1,11 +1,44 @@
 import React, {Component} from 'react'
-
-
+import moment from 'moment'
+import { Formik, Form, Field } from 'formik'
 
 class ClientComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            id : 1,
+            description : 'Learn Forms',
+            targetDate : moment(new Date()).format('YYYY-MM-DD')
+        }
+    }
+
     render() {
         return (
-            <div>Client Component for id - {this.props.match.params.id}</div>
+            <div>
+                <h1>Client</h1>
+                <div className="container">
+                    <Formik>
+                        {
+                            (props) => (
+                                <Form>
+                                    <fieldset className="form-group">
+                                        <label>Description</label>
+                                        <Field className="form-control" type="text" name="description"/>
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Target Date</label>
+                                        <Field className="form-control" type="date" name="targetDate"/>
+                                    </fieldset>
+                                    <button className="btn btn-success" type="submit">Save</button>
+                                </Form>
+                                
+                            )
+                        }
+                    </Formik>
+
+                </div>
+            </div>
         )
     }
 }
