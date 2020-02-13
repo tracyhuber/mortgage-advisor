73ffb84d477725bd5122bf616f740986ebede0aa
Tracy Huber <tmhuber3@gmail.com>
2020-02-12 18:42:39 -0600

    added validation to the Client form

--- src/components/mortgage/ClientComponent.jsx
+++ src/components/mortgage/ClientComponent.jsx
@@ -1,27 +1,65 @@
 import React, {Component} from 'react'
 import moment from 'moment'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 class ClientComponent extends Component {
 
     constructor(props) {
         super(props)
+
         this.state = {
-            id : 1,
-            description : 'Learn Forms',
+            id : this.props.match.params.id,
+            description : 'Learn Forms Now',
             targetDate : moment(new Date()).format('YYYY-MM-DD')
         }
+
+        this.onSubmit = this.onSubmit.bind(this)
+        this.validate = this.validate.bind(this)
+    }
+
+    validate(values) {
+        let errors = {}
+        if(!values.description) {
+            errors.description = 'Enter a Description'
+        } else if(values.description.length<5) {
+            errors.description = 'Enter at least 5 characters in description'
+        }
+
+        if(!moment(values.targetDate).isValid()) {
+            errors.targetDate = 'Enter a valid target date'
+        }
+
+        console.log(values)
+        return errors
+
+    }
+
+    onSubmit(values) {
+        console.log(values)
     }
 
     render() {
+        let {description, targetDate} = this.state
+        //let targetDate = this.state.targetDate
+
         return (
             <div>
                 <h1>Client</h1>
                 <div className="container">
-                    <Formik>
+                    <Formik
+                        initialValues={{description, targetDate}}
+                        onSubmit={this.onSubmit}
+                        validateOnChange={false}
+                        validateOnBlur={false}
+                        validate={this.validate}
+                    >
                         {
                             (props) => (
                                 <Form>
+                                    <ErrorMessage name="description" component="div" 
+                                            className="alert alert-warning"/>
+                                    <ErrorMessage name="targetDate" component="div" 
+                                            className="alert alert-warning"/>
                                     <fieldset className="form-group">
                                         <label>Description</label>
                                         <Field className="form-control" type="text" name="description"/>
